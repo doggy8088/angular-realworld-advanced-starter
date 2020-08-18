4903be7397ba0d9f822492ec880b648eceb84016
Will <doggy.huang@gmail.com>
2020-08-18 15:02:29 +0800

    任務02：建立並設定路由機制

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './layout/main/main.component';
+import { LoginComponent } from './auth/login/login.component';
+import { PostsComponent } from './posts/posts/posts.component';
+import { PostComponent } from './posts/post/post.component';
+import { EditorComponent } from './posts/editor/editor.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: MainComponent,
+    children: [
+      { path: '', pathMatch: 'full', redirectTo: 'posts' },
+      { path: 'posts', component: PostsComponent },
+      { path: 'post/:id', component: PostComponent },
+      { path: 'create', component: EditorComponent },
+    ]
+  },
+  { path: 'login', component: LoginComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
