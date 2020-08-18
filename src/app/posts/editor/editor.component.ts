c70f0a698e48f5716832db7cc09bdb006b50ae9c
Will <doggy.huang@gmail.com>
2020-08-18 16:32:02 +0800

    任務09：使用 Reactive Forms 繫結表單資料

--- src/app/posts/editor/editor.component.ts
+++ src/app/posts/editor/editor.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-editor',
@@ -6,10 +7,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./editor.component.css']
 })
 export class EditorComponent implements OnInit {
+  post: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.post = this.fb.group({
+      title: this.fb.control(''),
+      body: this.fb.control(''),
+      tags: this.fb.array([
+        this.fb.control('Angular'),
+        this.fb.control('HTML'),
+        this.fb.control('CSS'),
+      ])
+    });
+  }
+
+  get tags(): FormArray {
+    return this.post.get('tags') as FormArray;
+  }
+
+  addTag(tag) {
+    if (tag) {
+      this.tags.push(this.fb.control(tag));
+    }
+  }
+
+  createPost() {
+    if (this.post.valid) {
+      alert('表單送出');
+    }
   }
 
 }
