65c71d0ee32589af250a9ed1f5998ce9c4a799ff
Will <doggy.huang@gmail.com>
2020-08-18 15:27:21 +0800

    任務04：在程式中取得路由參數

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,18 @@ import { AppComponent } from './app.component';
 import { LayoutModule } from './layout/layout.module';
 
 import { HttpClientModule } from '@angular/common/http';
+import { PostsModule } from './posts/posts.module';
 
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule, LayoutModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    LayoutModule,
+    PostsModule,
+    AppRoutingModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/posts/post/post.component.ts
+++ src/app/posts/post/post.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-post',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostComponent implements OnInit {
 
-  constructor() { }
+  postId: number;
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      this.postId = +params.get('id');
+    });
   }
 
 }
